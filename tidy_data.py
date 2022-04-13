853aa3c9cc359b68940d15bfd480b34a5432e6ab
Stockard M17 <stockard.h@gmail.com>
2022-04-14 02:52:36 +0800

    match

--- tidy_data.py
+++ tidy_data.py
@@ -13,20 +13,83 @@
     3.26 - 今 @s
 []
 """
+import codecs
+import re
+from os import listdir
 
-'市卫健委今早（26日）通报：2022年3月25日0—24时，新增本土新冠肺炎确诊病例38例和无症状感染者2231例，其中5例确诊病例为此前无症状感染者转归，3例确诊病例和1773例无症状感染者在隔离管控中发现，其余在相关风险人群排查中发现。新增境外输入性新冠肺炎确诊病例9例和无症状感染者2例，均在闭环管控中发现。'
+def find_patterns(text, location_pattern, match_pattern, line_number, allow_skip = 0):
+    if location_pattern != '':
+        line_number = skip_lines(text, location_pattern, line_number)
+    values = re.findall(match_pattern, text[line_number])
+    if allow_skip:
+        if len(values) > 0:
+            values = values[0]
+        else:
+            values = ''
+    else:
+        values = values[0]
+#    print(text[line_number])
+    return line_number, values
 
-'其中本土无症状感染者102例，境外输入性无症状感染者8例。'
-'累计本土确诊707例，治愈出院476例，在院治疗224例，死亡7例。'
-'尚在医学观察中的无症状感染者8711例，其中本土无症状感染者8674'
+def skip_lines(text, location_pattern, line_number):
+    backup_line_number = line_number
+    try:
+        while len(re.findall(location_pattern, text[line_number]))==0:
+            line_number += 1
+    except:
+        line_number = backup_line_number
+    return line_number
 
-#清理确诊
-#2022年3月26日0—24时，新增本土新冠肺炎确诊病例45例。治愈出院17例。
-#病例1，居住于徐汇区，
-#闭环隔离管控人员/风险人群筛查
-#目前，已追踪到以上病例在本市的密切接触者53人，均已落实集中隔离观察。对病例曾活动过的场所已进行终末消毒。
-#清理无症状
-#2022年3月26日0—24时，新增本土无症状感染者2631例。
-#无症状感染者1—无症状感染者267，居住于浦东新区
-#闭环隔离管控/风险人群筛查
-#目前，已追踪到以上无症状感染者在本市的密切接触者783人，均已落实集中隔离观察。对无症状感染者曾活动过的场所已进行终末消毒。
+def cleaning_patients(text, text2):
+    pass
+
+def get_numbers(f, file):
+    """
+    ('时间', '确诊', '无症状', '无症状转确诊', '管控内确诊', '管控内无症状',\
+        '确诊密接', '无症状密接', '本土医学观察中无症状', '本土在院治疗中', '治愈出院', '重症', '死亡', '解除医学观察\n')    """
+    ALLOW_SKIP = 1
+    print(f'reading {file}')
+    text = f.readlines()
+    line_number = 0
+    line_number = skip_lines(text, '市卫健委', line_number) #跳到第一行
+    line_number, (patients, nosymptom) = find_patterns(text, '市卫健委', '新增本土新冠肺炎确诊病例(\d+)[例|]和无症状感染者(\d+)例', line_number)  #其中本土无症状感染者102例，境外输入性无症状感染者8例。
+    line_number, nosymptom_to_patient = find_patterns(text, '', '(\d+)例确诊病例为此前无症状感染者转归', line_number, ALLOW_SKIP)  #其中本土无症状感染者102例，境外输入性无症状感染者8例。
+    line_number, (patient_findallin_control, nosymptom_findallin_control) = find_patterns(text, '', '(\d+)例确诊病例和(\d+)例无症状感染者在隔离管控中', line_number)
+    #stage = ['patient', 'cleaning_patient', 'patient_close', 'nosymptom', 'cleaninng_nosymptom', 'nosymptom_close', 'end1', 'end2', 'end3']
+    line_number = skip_lines(text, '新增本土新冠肺炎确诊病例', line_number) #确诊
+    line_number = skip_lines(text, '病例(\d+)', line_number) #清理病人
+    while len(re.findall('密切接触者', text[line_number]))==0: #在密接之前持续清理
+        cleaning_patients(text[line_number], '确诊')
+        line_number += 1
+    line_number, patient_close = find_patterns(text, '', '已追踪到以上病例在本市的密切接触者(\d+)', line_number, ALLOW_SKIP) #已追踪到以上病例在本市的密切接触者53人
+    line_number = skip_lines(text, '新增本土无症状', line_number) #无症状
+    while len(re.findall('密切接触者', text[line_number]))==0: #在密接之前持续清理
+        cleaning_patients(text[line_number], '无症状')
+        line_number += 1
+    line_number, nosymptom_close = find_patterns(text, '', '已追踪到以上无症状感染者在本市的密切接触者(\d+)', line_number, ALLOW_SKIP) #已追踪到以上病例在本市的密切接触者53人
+    line_number, leave_nosymptom_control = find_patterns(text, '解除医学观察', '本土无症状感染者(\d+)', line_number, ALLOW_SKIP)  #其中本土无症状感染者102例，境外输入性无症状感染者8例。
+    line_number, heal = find_patterns(text, '在院治疗', '治愈出院(\d+)', line_number)  #累计本土确诊707例，治愈出院476例，在院治疗224例，死亡7例。
+    line_number, in_hospital = find_patterns(text, '', '在院治疗(\d+)', line_number)  #累计本土确诊707例，治愈出院476例，在院治疗224例，死亡7例。
+    line_number, serious = find_patterns(text, '', '重症(\d+)', line_number, ALLOW_SKIP)  #累计本土确诊707例，治愈出院476例，在院治疗224例，死亡7例。
+    line_number, dead = find_patterns(text, '', '死亡(\d+)', line_number, ALLOW_SKIP)  #累计本土确诊707例，治愈出院476例，在院治疗224例，死亡7例。
+    line_number, (i, nosymptom_control) = find_patterns(text, '尚在医学观察中的', '尚在医学观察中的无症状感染者(\d+)例，其中本土无症状感染者(\d+)', line_number)  #尚在医学观察中的无症状感染者8711例，其中本土无症状感染者8674
+    result = (file, patients, nosymptom, nosymptom_to_patient, patient_findallin_control, nosymptom_findallin_control,\
+            patient_close, nosymptom_close, nosymptom_control, in_hospital, heal, serious, dead, leave_nosymptom_control)
+    print(result)
+    return result
+
+if __name__ == '__main__':
+    dir = 'data/number/'
+    data_file = 'data/use/macro_data.csv'
+    begin = 20220325
+    filelist = listdir(dir)
+    w = codecs.open(data_file, mode = 'w', encoding='utf_8')
+    w.write(",".join(('时间', '确诊', '无症状', '无症状转确诊', '管控内确诊', '管控内无症状',\
+            '确诊密接', '无症状密接', '本土医学观察中无症状', '本土在院治疗中', '治愈出院', '重症', '死亡', '解除医学观察\n')))
+    for file in filelist:
+        if file.find('.') > 0 or int(file) < begin:
+            continue
+        f = codecs.open(dir + file, mode = 'r', encoding='utf_8')
+        result = get_numbers(f, file)
+        w.write(",".join(result) + '\n')
+    w.close()
