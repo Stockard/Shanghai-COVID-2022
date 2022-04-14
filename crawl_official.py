68e7be5a62e4e5ffae87a537a2ca8e7cf5896a56
Stockard M17 <stockard.h@gmail.com>
2022-04-15 03:35:50 +0800

    build_geo_compare
    每日新增地址有限，实际只需要再抓取少量即可

---
+++ crawl_official.py
@@ -0,0 +1,104 @@
+#encoding=utf-8
+#修改自 https://github.com/youqingxiaozhua/GeneralNewsSpider
+#清理上海发布每天发布的病例信息 since 3/26/2022 | 3月17日前每个病人都有地址 ｜ 3月18-25日每个病人都有区
+#5号开始地址数据变成微信数据，无法获取
+#3月17日	https://mp.weixin.qq.com/s/ZSIDH6G-IIrWUmXKpWGulg
+#3月16日	https://mp.weixin.qq.com/s/J8Ib1MFCWI6vouw5Gz2MiQ
+#3月18日	https://mp.weixin.qq.com/s/OFt7LzeHt8fNl6GqPpkD1g
+#3月18日	https://mp.weixin.qq.com/s/xLVPnOTErTe3dmAenUyDGQ
+#https://wsjkw.sh.gov.cn/yqtb/index.html
+
+import re
+import codecs
+from os import listdir
+
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+from newspaper import Article
+from retrying import retry
+from config import address_folder, number_folder
+
+baseurl = ['https://wsjkw.sh.gov.cn/yqtb/index.html']
+prefixurl = 'https://wsjkw.sh.gov.cn/'
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36",
+}
+
+def crawl_list(baseurl):
+    """
+    解析列表
+    """
+    date_pattern = '((\d{1,2})月(\d{1,2})日)'
+    address_pattern = '居住地信息'
+
+    r = requests.get(baseurl, headers = headers)
+    if r.status_code == 200:
+        #获取列表
+        print('--get list--')
+        html = r.content
+        soup = BeautifulSoup(html, features="lxml")
+        filter_list = []
+        wide_list = soup.find_all('li')
+        for i in wide_list:
+            dates = re.findall(date_pattern, i.text)
+            if len(dates) >= 1:
+                create_date = dates[0][0]
+                date, month = re.findall('\d+', create_date)
+                create_date = ("2022%02d%02d" % (int(date), int(month)))
+                titles = i.find_all('a')
+                for j in titles:
+                    title = j.text
+                    if len(title) != 0:
+                        break
+                if len(title) == 0:
+                    continue
+                try:
+                    detail_url = i.find('a').get('href')
+                except:
+                    continue
+                is_address = len(re.findall(address_pattern, i.text))
+                filter_list.append((is_address, create_date, urljoin(prefixurl, detail_url), i.text))
+        for is_address, create_date, url, title in filter_list:
+            if not not_crawled(url):
+                continue
+            title, article = parse_notice_detail(url, title)
+            if is_address:
+                save(address_folder, create_date, article)
+            else:
+                save(number_folder, create_date, article)
+
+
+@retry(stop_max_attempt_number=9, wait_random_min=10, wait_random_max=20)
+def parse_notice_detail(url, title):
+    """
+    解析新闻正文
+    :param url: 完整的详情页url
+    :param title: 列表页解析到的标题
+    :return: tuple: title, content
+    """
+    article = Article(url, language='zh')
+    try:
+        article.download()
+        article.parse()
+    except Exception as e:
+        if 'failed with 404' in str(e):
+            raise Page404Error
+    return title.strip(), article.text
+
+def not_crawled(url):
+    #检查是否在已经抓取列表里
+    pass
+    return True
+
+def save(dir, create_date, article):
+    #抓取并保存
+    filelist = listdir(dir)
+    if create_date not in filelist:
+        f = codecs.open(dir + '/' + create_date, mode='w', encoding='utf_8')
+        f.write(article)
+        f.close()
+
+if __name__ == '__main__':
+    for url in baseurl:
+        crawl_list(url)
--- tidy_address.py
+++ tidy_address.py
@@ -1,3 +1,39 @@
 #placeholder
 #[] 根据temp/address_daily里的数据汇总到经纬度-地址对应表：经纬度只需查询新增地址
 #先处理好旧数据，再加增量数据
+import pandas as pd
+import codecs
+from config import address_file, geo_file, address_add_file, address_file_extend
+
+def list_compare(master, full):
+    add_list = []
+    for i in full:
+        if i not in master:
+            add_list.append(i)
+    return add_list
+
+def complete_check():
+    #检查地址数据完整性
+    #placeholder
+    pass
+
+if __name__ == '__main__':
+    address_add = codecs.open(address_add_file, mode = 'w', encoding='utf_8')
+
+    address = pd.read_csv(address_file, encoding = 'utf-8')
+    address_extend = pd.read_csv(address_file_extend, encoding = 'utf-8')
+    address_series = address.address.append(address_extend.address, ignore_index = True)
+    address_full_list = address_series.unique()
+
+    geo = pd.read_csv(geo_file, encoding = 'utf-8')
+    geo_full_list = pd.unique(geo.address)
+
+    print(f'--address list unique {len(address_full_list)} full {address_series.shape[0]}--')
+    print(f'--geo list unique {len(geo_full_list)} full {geo.shape[0]}--')
+
+    address_add_list = list_compare(geo_full_list, address_full_list)
+    address_add.write('address\n')
+    for i in address_add_list:
+        address_add.write(i)
+        address_add.write('\n')
+    address_add.close()
